667b934d435e03d8eb9e392f527e93e033c74b73
kiorky <kiorky@cryptelium.net>
2010-02-27 17:07:13 +0100

    initial

---
+++ README.txt
@@ -0,0 +1,14 @@
+******************************************************************************
+Recipe for compiling and installing software with or without minitage
+******************************************************************************
+
+.. contents::
+
+=======================
+Introduction
+=======================
+
+
+register branches to ohloh and related stuff
+
+
---
+++ setup.py
@@ -0,0 +1,61 @@
+import os, sys
+from setuptools import setup, find_packages
+setupdir = os.path.abspath(
+    os.path.dirname(__file__)
+)
+os.chdir(setupdir)
+
+name='minitage.ohloh'
+version = '1.0'
+
+def read(*rnames):
+    return open(
+        os.path.join(setupdir, *rnames)
+    ).read()
+
+long_description = (
+    read('README.txt')
+    + '\n'\
+    + read('CHANGES.txt')
+    + '\n'
+)
+
+
+long_description = 'ohloh minitage helpers'
+
+setup(
+    name=name,
+    version=version,
+    description="",
+    long_description= long_description,
+    classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='ohloh',
+    author='Mathieu Pasquet',
+    author_email='kiorky@cryptelium.net',
+    url='http://cheeseshop.python.org/pypi/%s' % name,
+    license='BSD',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['minitage', name],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires = [
+        'zc.buildout',
+        'lxml',
+        'windmill'
+    ],
+    extras_require={'test': ['IPython', 'zope.testing', 'mocker']},
+    entry_points = {
+        'console_scripts' : [
+            'ohloh.register_repo = %s.ohloh:register_repo' % name
+        ]
+    },
+)
+
+
